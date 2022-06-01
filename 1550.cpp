bb9275ce4a103ad63435e4e97aed564c41bb4883
Ha_sang_wook <64821752+Cre-Eve@users.noreply.github.com>
2022-06-02 02:02:20 +0900

    Bronze 5

---
+++ 1550.cpp
@@ -0,0 +1,21 @@
+//BaekJoon_1550
+//16진수
+
+/*
+* 제한 시간 : 0ms / 2s
+* 메모리 제한 : 2020 KB/ 128 MB
+* 정답 비율 : 64.962%
+*/
+
+#include <iostream>
+
+int main()
+{
+    int num = 0;
+
+    scanf("%x", &num);
+
+    printf("%d", num);
+ 
+    return 0;
+}
