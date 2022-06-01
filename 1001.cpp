30326d85ada25b478bcbea5dbb061edafd87d791
Ha_sang_wook <64821752+Cre-Eve@users.noreply.github.com>
2022-06-02 01:03:12 +0900

    Create 1001.cpp

---
+++ 1001.cpp
@@ -0,0 +1,20 @@
+//BaekJoon_1000
+//A+B
+
+/*
+* 제한 시간 : 2s
+* 메모리 제한 : 128MB
+* 정답 비율 : 42.042%
+*/
+
+#include <iostream>
+
+int main()
+{
+    int A = 0, B = 0;
+
+    std::cin >> A >> B;
+    std::cout << A - B;
+
+    return 0;
+}
