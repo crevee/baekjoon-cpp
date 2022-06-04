3aa9274bd4e51e7f74a39933e8218d1d20dfa6b0
Ha_sang_wook <64821752+Cre-Eve@users.noreply.github.com>
2022-06-05 01:19:07 +0900

    Bronze 5

---
+++ Bronze/10171.cpp
@@ -0,0 +1,20 @@
+//BaekJoon_10171
+//나부 함대 데이터
+
+/*
+* 제한 시간 : 0ms / 1s
+* 메모리 제한 : 2020KB / 256MB
+* 정답 비율 : 32.743%
+*/
+
+#include <iostream>
+
+int main(){
+
+	printf("\\    /\\\n");
+	printf(" )  ( ')\n");
+	printf("(  /  )\n");
+	printf(" \\(__)|\n");
+
+	return 0;
+}
