94d9cf1a97b168ba6402427def961f59eefe77b3
Ha_sang_wook <64821752+Cre-Eve@users.noreply.github.com>
2022-06-05 00:15:34 +0900

    Bronze 5

---
+++ Bronze/3046.cpp
@@ -0,0 +1,18 @@
+//BaekJoon_3046
+//R2
+
+/*
+* 제한 시간 : 0ms / 1s
+* 메모리 제한 : 2020KB / 128MB
+* 정답 비율 : 79.787%
+*/
+
+#include <iostream>
+
+int main() {
+
+	int R1 = 0, R2 = 0, S = 0 ;
+
+	scanf("%d %d", &R1, &S);
+	printf("%d", 2 * S - R1);
+}
