6a998e79a3363a63abb10ab3cb9f4684bdf7139f
Ha_sang_wook <64821752+Cre-Eve@users.noreply.github.com>
2022-06-05 00:41:12 +0900

    Bronze 5

---
+++ Bronze/7287.cpp
@@ -0,0 +1,15 @@
+//BaekJoon_7287
+//등록
+
+/*
+* 제한 시간 : 0ms / 1s
+* 메모리 제한 : 2020KB / 128MB
+* 정답 비율 : 50.063%
+*/
+#include <iostream>
+
+int main() {
+
+	printf_s("맞은문제수 \n");
+	printf_s("닉네임");
+}
