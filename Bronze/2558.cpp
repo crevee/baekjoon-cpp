915b0cb91e74991c78101b67b441d732aaf1b2a4
Ha_sang_wook <64821752+Cre-Eve@users.noreply.github.com>
2022-06-04 23:25:57 +0900

    Brozne 5

---
+++ Bronze/2558.cpp
@@ -0,0 +1,18 @@
+//BaekJoon_2558
+//A + B - 2
+
+/*
+* 제한 시간 : 0ms / 1s
+* 메모리 제한 : 2020KB / 128MB
+* 정답 비율 : 73.695%
+*/
+
+#include <iostream>
+
+int main() {
+
+	int A = 0, B = 0;
+
+	std::cin >> A >> B;
+	std::cout << A + B;
+}
