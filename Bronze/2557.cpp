912755450ac5929ac81e5a3b988809e98cf6443d
Ha_sang_wook <64821752+Cre-Eve@users.noreply.github.com>
2022-06-04 23:22:42 +0900

    Bronze 5

---
+++ Bronze/2557.cpp
@@ -0,0 +1,15 @@
+//BaekJoon_2557
+//Hello World
+
+/*
+* 제한 시간 : 0ms / 1s
+* 메모리 제한 : 2020KB / 128MB
+* 정답 비율 : 40.003%
+*/
+
+#include <iostream>
+
+int main() {
+	
+	std::cout << "Hello World!" << std::endl;
+}
