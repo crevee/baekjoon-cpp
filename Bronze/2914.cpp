03bc041fb19b6de514511b4195859f2f06b3cef7
Ha_sang_wook <64821752+Cre-Eve@users.noreply.github.com>
2022-06-04 23:58:09 +0900

    Bronze 5

---
+++ Bronze/2914.cpp
@@ -0,0 +1,18 @@
+//BaekJoon_2914
+//저작권
+
+/*
+* 제한 시간 : 0ms / 1s
+* 메모리 제한 : 2020KB / 128MB
+* 정답 비율 : 79.739%
+*/
+
+#include <iostream>
+
+int main() {
+
+	int A = 0, I = 0;
+
+	scanf("%d %d", &A, &I);
+	printf("%d", A * (I-1) +1);
+}
