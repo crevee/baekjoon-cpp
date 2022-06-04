2905dc51465db37c561c9944bb54ace51781f64f
Ha_sang_wook <64821752+Cre-Eve@users.noreply.github.com>
2022-06-05 00:29:08 +0900

    Bronze 5

---
+++ Bronze/5339.cpp
@@ -0,0 +1,24 @@
+//BaekJoon_5339
+//콜센터
+
+/*
+* 제한 시간 : 0ms / 1s
+* 메모리 제한 : 2020KB / 128MB
+* 정답 비율 : 67.543%
+*/
+
+#include <iostream>
+
+int main() {
+
+	printf("     /~\\\n");
+	printf("    ( oo|\n");
+	printf("    _\\=/_\n");
+	printf("   /  _  \\\n");
+	printf("  //|/.\\|\\\\\n");
+	printf(" ||  \\ /  ||\n");
+	printf("============\n");
+	printf("|          |\n");
+	printf("|          |\n");
+	printf("|          |\n");
+}
