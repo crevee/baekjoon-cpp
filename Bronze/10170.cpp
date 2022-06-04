c41f9ca5de5be94c57c3f218af13f5c403de1e53
Ha_sang_wook <64821752+Cre-Eve@users.noreply.github.com>
2022-06-05 01:16:29 +0900

    Bronze 5

---
+++ Bronze/10170.cpp
@@ -0,0 +1,29 @@
+//BaekJoon_10170
+//NFC West vs North
+
+/*
+* 제한 시간 : 0ms / 1s
+* 메모리 제한 : 2020KB / 256MB
+* 정답 비율 : 72.541%
+*/
+
+#include <iostream>
+
+int main(){
+
+	printf("NFC West       W   L  T\n");
+	printf("-----------------------\n");
+	printf("Seattle        13  3  0\n");
+	printf("San Francisco  12  4  0\n");
+	printf("Arizona        10  6  0\n");
+	printf("St. Louis      7   9  0\n\n");
+	printf("NFC North      W   L  T\n");
+	printf("-----------------------\n");
+	printf("Green Bay      8   7  1\n");
+	printf("Chicago        8   8  0\n");
+	printf("Detroit        7   9  0\n");
+	printf("Minnesota      5  10  1");
+
+
+	return 0;
+}
