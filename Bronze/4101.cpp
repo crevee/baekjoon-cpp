713ec68b576ab1444aacd1da6fb5182cf8d2b4b3
Ha_sang_wook <64821752+Cre-Eve@users.noreply.github.com>
2022-06-06 19:36:01 +0900

    Bronze 5

---
+++ Bronze/4101.cpp
@@ -0,0 +1,33 @@
+//BaekJoon_4101
+//크냐?
+
+/*
+* 제한 시간 : 0ms / 1s
+* 메모리 제한 : 2020KB / 128MB
+* 정답 비율 : 54.035%
+*/
+
+#include <iostream>
+
+int main(){
+
+	int A = 1, B = 1;
+
+	while(1)
+	{
+			scanf_s("%d %d", &A, &B);
+
+			if (A == 0 && B == 0) {
+				break;
+			}
+			if (A > B)
+			{
+				printf("Yes\n");
+			}
+			else
+			{
+				printf("No\n");
+			}
+	}
+	return 0;
+}
