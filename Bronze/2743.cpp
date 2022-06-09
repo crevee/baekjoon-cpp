1518fdbf5e0f74a23dd3307cd2cbeef481a9dc4b
Ha_sang_wook <64821752+Cre-Eve@users.noreply.github.com>
2022-06-09 19:51:44 +0900

    Bronze 5

---
+++ Bronze/2743.cpp
@@ -0,0 +1,20 @@
+//BaekJoon_2743
+//단어 길이 재기
+
+/*
+* 제한 시간 : 0ms / 1s
+* 메모리 제한 : 2020KB / 128MB
+* 정답 비율 : 84.143%
+*/
+
+#include <iostream>
+#include <string>
+
+int main() {
+
+	std::string s;
+	std::cin >> s;
+	std::cout << s.size() << "\n";
+
+	return 0;
+}
