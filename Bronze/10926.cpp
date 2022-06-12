dba03942fd0638be088f578bf809820b86742919
Ha_sang_wook <64821752+Cre-Eve@users.noreply.github.com>
2022-06-12 22:58:15 +0900

    Bronze 5

---
+++ Bronze/10926.cpp
@@ -0,0 +1,21 @@
+//BaekJoon_10926
+//??!
+
+/*
+* 제한 시간 : 0ms / 1s
+* 메모리 제한 : 2024KB / 256MB
+* 정답 비율 : 53.249%
+*/
+
+#include <iostream>
+
+int main() {
+
+	std::string ID;
+
+	std::cin >> ID;
+	
+	std::cout << ID << "??!"<< "\n";
+
+	return 0;
+}
