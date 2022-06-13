9e70e4f51fa6c693a5fc620c5ea9272432df7248
Ha_sang_wook <64821752+Cre-Eve@users.noreply.github.com>
2022-06-13 21:08:44 +0900

    Bronze 5

---
+++ Bronze/10718.cpp
@@ -0,0 +1,18 @@
+//BaekJoon_10718
+//We love kriii
+
+/*
+* 제한 시간 : 0ms / 1s
+* 메모리 제한 : 2020KB / 256MB
+* 정답 비율 : 65.117%
+*/
+
+#include <iostream>
+
+int main() {
+
+	std::cout << "강한친구 대한육군" << std::endl;
+	std::cout << "강한친구 대한육군";
+
+	return 0;
+}
