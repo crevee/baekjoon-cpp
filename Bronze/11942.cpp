87930177566bd9ebba519aceed10940cbcfc175d
Ha_sang_wook <64821752+Cre-Eve@users.noreply.github.com>
2022-06-15 23:31:23 +0900

    Bronze 5

---
+++ Bronze/11942.cpp
@@ -0,0 +1,17 @@
+//BaekJoon_11942
+//고려대는 사랑입니다.
+
+/*
+* 제한 시간 : 0ms / 1s
+* 메모리 제한 : 2020KB / 32MB
+* 정답 비율 : 84.690%
+*/
+
+#include <iostream>
+
+int main() {
+
+	std::cout << "고려대학교";
+
+	return 0;
+}
