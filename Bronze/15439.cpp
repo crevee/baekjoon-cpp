e3d76bd0fd948c255f3f9de025607cac2c6e39ba
Ha_sang_wook <64821752+Cre-Eve@users.noreply.github.com>
2022-06-27 23:51:12 +0900

    Bronze 4

---
+++ Bronze/15439.cpp
@@ -0,0 +1,23 @@
+//BaekJoon_15439
+//Vera and Outfits
+
+/*
+* 제한 시간 : 0ms / 2s
+* 메모리 제한 : 2020KB / 256MB
+* 정답 비율 : 89.337%
+*/
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+   
+    int N = 0 ;
+    
+    std::cin >> N;
+    
+    std::cout << N * (N-1) << std::endl;
+    
+    
+    
+    return 0;
+}
