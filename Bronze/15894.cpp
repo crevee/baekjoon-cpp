b8c4267b7d5739d3a7699839c0fbd35dc1a08f4e
Ha_sang_wook <64821752+Cre-Eve@users.noreply.github.com>
2022-07-09 00:27:18 +0900

    Bronze 3

---
+++ Bronze/15894.cpp
@@ -0,0 +1,27 @@
+//BaekJoon_15894
+//수학은 체육과목 입니다
+
+/*
+* 제한 시간 : 0ms / 1s
+* 메모리 제한 : 2020KB / 512MB
+* 정답 비율 : 55.550%
+*/
+
+#include <iostream>
+
+int main() {
+
+	std::ios::sync_with_stdio(0);
+	std::cin.tie(0);
+	
+	std::cout << std::fixed;
+	std::cout.precision(0);
+
+
+	double n;
+
+	std::cin >> n;
+	std::cout << n * 4;
+
+	return 0;
+}
