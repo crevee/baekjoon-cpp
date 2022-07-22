52ea53daf4c10f808513a7b10a36b7f3f9dc78e8
Ha_sang_wook <64821752+Cre-Eve@users.noreply.github.com>
2022-07-22 21:48:00 +0900

    Bronze 1

---
+++ Bronze/1037.cpp
@@ -0,0 +1,59 @@
+//BaekJoon_1037.cpp
+//약수
+
+/*
+* 제한 시간 : 0ms / 2s
+* 메모리 제한 : 2020KB / 512MB
+* 정답 비율 : 53.325%
+*/
+
+#include <iostream>
+
+
+int main() {
+
+	std::ios::sync_with_stdio(0);
+	std::cin.tie(0);
+
+
+	int count = 0, result = 0;
+	int* divisor;
+
+
+	std::cin >> count;
+
+	divisor = new int[count];
+
+
+	for(int i = 0; i < count; i++)
+	{
+		std::cin >> divisor[i];
+	}
+
+	for (int i = 0; i < count-1; i++)
+	{
+		for (int j = i+ 1; j < count; j++)
+		{
+			if (divisor[i] > divisor[j])
+			{
+				int temp = divisor[i];
+				divisor[i] = divisor[j];
+				divisor[j] = temp;
+			}
+		}
+	}
+
+	if (count == 1)
+	{
+		result = divisor[0] * divisor[0];
+	}
+	else
+	{
+		result = divisor[0] * divisor[count - 1];
+	}
+
+	std::cout << result;
+
+	return 0;
+
+}
