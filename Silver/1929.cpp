dee9be0f16acd71d7eeda403447d9d5059f943ba
Ha_sang_wook <64821752+Cre-Eve@users.noreply.github.com>
2022-07-28 00:48:28 +0900

    Silver 3
    문제 해결 방법 추가 및 상황에 따른 시간과 메모리 차이 추가

--- Silver/1929.cpp
+++ Silver/1929.cpp
@@ -8,7 +8,7 @@
 */
 
 
-##방법 1 시간 188ms / 메모리 2020KB
+// 방법 1 시간 188ms / 메모리 2020KB
 
 #include <iostream>
 
@@ -44,3 +44,49 @@ int main() {
 	}
 	return 0;
 }
+
+
+// 방법 2 시간 16ms / 메모리 2980KB
+
+// delete [] prime_array; 추가시 시간 4ms 추가됨
+// bool 배열 int 배열로 변환시 메모리 2880KB 추가됨
+
+#include <iostream>
+
+int main() {
+
+	std::ios::sync_with_stdio(0);
+	std::cin.tie(0);
+
+
+	int m, n;
+	bool* prime_array;
+
+
+	std::cin >> m >> n;
+
+	prime_array = new bool [n + 1] {};
+	std::fill_n(prime_array, n + 1, 1);
+
+	prime_array[0] = prime_array[1] = 0;
+
+	for (int i = 2; i * i <= n; i++)
+	{
+		for (int j = i * i; j <= n; j+=i)
+		{
+			prime_array[j] = 0;
+		}
+	}
+
+	for (int i = m; i <= n; i++)
+	{
+		if (prime_array[i] == 1)
+		{
+			std::cout << i << '\n';
+		}
+	}
+
+	return 0;
+
+}
+
