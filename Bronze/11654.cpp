2a46de57e2f66ef579b3c8270f4f2c859f48c29c
Ha_sang_wook <64821752+Cre-Eve@users.noreply.github.com>
2022-08-11 09:06:57 +0900

    Bronze 5

---
+++ Bronze/11654.cpp
@@ -0,0 +1,27 @@
+//BaekJoon_11654
+//아스키 코드
+
+/*
+* 제한 시간 : 0ms / 1s
+* 메모리 제한 : 2020KB / 256MB
+* 정답 비율 : 80.117%
+*/
+
+#include <iostream>
+
+int main()
+{
+	std::ios::sync_with_stdio(0);
+	std::cin.tie(0);
+
+	char c;
+	int n;
+
+	std::cin >> c;
+
+	n = c;
+
+	std::cout << n;
+
+	return 0;
+}
