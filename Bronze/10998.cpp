516aceb62495babf6dd9e7518b1540b4637d19a1
Ha_sang_wook <64821752+Cre-Eve@users.noreply.github.com>
2022-08-11 10:06:30 +0900

    Bronze 5

---
+++ Bronze/10998.cpp
@@ -0,0 +1,23 @@
+//BaekJoon_10998
+//A x B
+
+/*
+* 제한 시간 : 0ms / 1s
+* 메모리 제한 : 2020KB / 256MB
+* 정답 비율 : 78.445%
+*/
+
+#include <iostream>
+
+int main()
+{
+	std::ios::sync_with_stdio(0);
+	std::cin.tie(0);
+
+	int a, b;
+
+	std::cin >> a >> b;
+	std::cout << a * b;
+
+	return 0;
+}
