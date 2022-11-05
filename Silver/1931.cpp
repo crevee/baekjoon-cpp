8bbfeb69ce5879fcc658fa00d510b25dbba5f48f
Ha sang wook <64821752+Cre-Eve@users.noreply.github.com>
2022-11-05 23:05:29 +0900

    Silver 1

---
+++ Silver/1931.cpp
@@ -0,0 +1,46 @@
+//BaekJoon_1931
+//회의실 배정
+
+/*
+* 제한 시간 : 0ms / 2s
+* 메모리 제한 : 2020KB / 128MB
+* 정답 비율 : 29.580%
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main() {
+
+	std::ios::sync_with_stdio(0);
+	std::cin.tie(0);
+
+	int n, start, end;
+	std::vector<std::pair<int, int>> schedule;
+
+
+
+	std::cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		std::cin >> start >> end;
+		schedule.push_back(std::make_pair(end, start));
+	}
+	
+	std::sort(schedule.begin(), schedule.end());
+
+	int time = schedule[0].first;
+	int count = 1;
+
+	for (int i = 1; i < n; i++) {
+		if (time <= schedule[i].second) {
+			count++;
+			time = schedule[i].first;
+		}
+	}
+
+	std::cout << count;
+
+	return 0;
+}
