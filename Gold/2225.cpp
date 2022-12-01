96037982fc9ae304fa9ff4f19e2fe0f02101bdfd
Ha sang wook <64821752+Cre-Eve@users.noreply.github.com>
2022-12-01 23:59:17 +0900

    Gold 5

---
+++ Gold/2225.cpp
@@ -0,0 +1,36 @@
+//BaekJoon_2225
+//합분해
+
+/*
+* 제한 시간 : 0ms / 2s
+* 메모리 제한 : 2024KB / 128MB
+* 정답 비율 : 42.702%
+*/
+
+#include <iostream>
+
+long d[201][201];
+
+int main() {
+
+	std::ios::sync_with_stdio(0);
+	std::cin.tie(0);
+
+	int n, k;
+
+	std::cin >> n >> k;
+
+	for (int i = 0; i <= k; i++) {
+		d[1][i] = i;
+	}
+
+	for (int i = 2; i <= n; i++) {
+		for (int j = 1; j <= k; j++) {
+			d[i][j] = (d[i - 1][j] + d[i][j - 1]) % 1000000000;
+		}
+	}
+	
+	std::cout << d[n][k] << "\n";
+
+	return 0;
+}
