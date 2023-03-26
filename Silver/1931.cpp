56c37e5b8c5bea38c21ac8838864b50beff9a624
Ha Sangwook <64821752+crevee@users.noreply.github.com>
2023-03-27 01:58:02 +0900

    Silver 1

--- Silver/1931.cpp
+++ Silver/1931.cpp
@@ -44,3 +44,46 @@ int main() {
 
 	return 0;
 }
+
+/*
+* 제한 시간 : 24ms / 2s
+* 메모리 제한 : 2808KB / 128MB
+* 정답 비율 : 29.580%
+*/
+
+/*
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n;
+	cin >> n;
+
+	vector<pair<int, int>> a(n);
+
+	for (int i = 0; i < n; i++) {
+		cin >> a[i].second;
+		cin >> a[i].first;
+	}
+
+	sort(a.begin(), a.end());
+
+	int count = 0;
+	int end = -1;
+
+	for (int i = 0; i < n; i++) {
+		if (a[i].second >= end) {
+			end = a[i].first;
+			count++;
+		}
+	}
+	cout << count << "\n";
+}
+*/
