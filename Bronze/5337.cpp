efe53849bf81c7bcbc932c0aaa202b73406cfacf
Ha Sangwook <64821752+crevee@users.noreply.github.com>
2023-05-08 23:55:40 +0900

    Bronze 5

---
+++ Bronze/5337.cpp
@@ -0,0 +1,19 @@
+//BaekJoon_5337
+//웰컴
+
+/*
+* 제한 시간 : 0ms / 1s
+* 메모리 제한 : 2020KB / 128MB
+* 정답 비율 : 70.705%
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+	cout << ".  .   ." << "\n";
+	cout << "|  | _ | _. _ ._ _  _" << "\n";
+	cout << "|/\\|(/.|(_.(_)[ | )(/.";
+}
