8b629fffbeda119a9c2bc72aea93a6e4a6c291f7
Ha Sangwook <64821752+crevee@users.noreply.github.com>
2023-06-19 00:12:02 +0900

    Bronze 4

---
+++ Bronze/24723.cpp
@@ -0,0 +1,20 @@
+//BaekJoon_24723
+//녹색거탑
+
+/*
+* 제한 시간 : 0ms / 2.022s
+* 메모리 제한 : 2020KB / 319MB
+* 정답 비율 : 66.937%
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int n;
+
+int main(){
+    cin >> n;
+  
+    cout << (1 << n);
+}
