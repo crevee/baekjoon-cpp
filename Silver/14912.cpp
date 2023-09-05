50e5c1d09dbfc4dc173a5232f666d75a48badef1
Ha Sangwook <64821752+crevee@users.noreply.github.com>
2023-09-05 23:57:05 +0900

    Silver 5

---
+++ Silver/14912.cpp
@@ -0,0 +1,46 @@
+//BaekJoon_14912
+//숫자빈도수
+
+
+/*
+* 제한 시간 : 0ms / 2s
+* 메모리 제한 : 2020KB / 512MB
+* 정답 비율 : 80.714%
+*/
+
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int c, r, k, x, y, count = 1;
+	int dir[4][2] = { {0, -1}, {1, 0}, {0, 1}, {-1, 0} }, d = 0;
+
+	cin >> c >> r >> k;
+
+	if (k > c * r) {
+		cout << 0;
+	}
+	else {
+		vector<vector<int>>seat(r, vector<int>(c, 0));
+		y = r - 1, x = 0;
+
+		while (count < k) {
+			int dx = x + dir[d][0];
+			int dy = y + dir[d][1];
+
+			if (dx < 0 || dx >= c || dy < 0 || dy >= r || seat[dy][dx] != 0) {
+				d = (d + 1) % 4;
+				continue;
+			}
+			seat[y][x] = count++;
+			y = dy, x = dx;
+		}
+		cout << x + 1 << " " << r - y;
+	}
+}
