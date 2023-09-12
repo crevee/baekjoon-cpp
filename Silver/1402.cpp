84e95c7d81d6fb9ad9f42e437bca1d7f453e0217
Ha Sangwook <64821752+crevee@users.noreply.github.com>
2023-09-13 00:35:46 +0900

    Silver 5

---
+++ Silver/1402.cpp
@@ -0,0 +1,29 @@
+//BaekJoon_1402
+//아무래도이문제는A번난이도인것같다
+
+
+/*
+* 제한 시간 : 0ms / 2s
+* 메모리 제한 : 2020KB / 128MB
+* 정답 비율 : 47.337%
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main(void) {
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int t, a, b;
+
+	cin >> t;
+
+	for (int i = 0; i < t; i++) {
+		cin >> a >> b;
+
+		cout << "yes" << '\n';
+	}
+}
