833c7a0bf4c1976c281eafc13b9d3405832894ce
Ha Sangwook <64821752+crevee@users.noreply.github.com>
2023-09-29 23:55:14 +0900

    Bronze 4

---
+++ Bronze/24263.cpp
@@ -0,0 +1,24 @@
+//BaekJoon_24263
+//알고리즘 수업 - 알고리즘의 수행 시간 2
+
+/*
+* 제한 시간 : 0ms / 1s
+* 메모리 제한 : 2020KB / 512MB
+* 정답 비율 : 79.874%
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main(void) {
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n;
+
+	cin >> n;
+
+	cout << n << '\n' << 1 << '\n';
+}
