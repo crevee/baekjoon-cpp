bee518aabfb6e76e57109d50aeb8a88ba28fd181
Ha Sangwook <64821752+crevee@users.noreply.github.com>
2024-01-09 15:32:01 +0900

    Bronze 5

---
+++ Bronze/11382.cpp
@@ -0,0 +1,25 @@
+//BaekJoon_11382
+//꼬마 정민
+
+/*
+* 제한 시간 : 0ms / 1s
+* 메모리 제한 : 2020KB / 512MB
+* 정답 비율 : 41.238%
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	long long a, b, c;
+
+	cin >> a >> b >> c;
+
+	cout << a + b + c;
+
+	return 0;
+}
