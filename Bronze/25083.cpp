2c65c493ab527a25a22da8bfa1e7ff52f34cea4a
Ha Sangwook <64821752+crevee@users.noreply.github.com>
2024-01-09 15:29:06 +0900

    Bronze 5

---
+++ Bronze/25083.cpp
@@ -0,0 +1,26 @@
+//BaekJoon_25083
+//새싹
+
+/*
+* 제한 시간 : 0ms / 1s
+* 메모리 제한 : 2020KB / 1024MB
+* 정답 비율 : 52.428%
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cout << "         ,r'\"7" << '\n';
+	cout << "r`-_   ,'  ,/" << '\n';
+	cout << " \\. \". L_r'" << '\n';
+	cout << "   `~\\/" << '\n';
+	cout << "      |" << '\n';
+	cout << "      |";
+
+	return 0;
+}
