f2e0ee0705bacb609fb81914a3b977e7592c5164
Ha Sangwook <64821752+crevee@users.noreply.github.com>
2024-01-10 01:52:34 +0900

    Silver 3

---
+++ Silver/15652.cpp
@@ -0,0 +1,47 @@
+//BaekJoon_15652
+//N과 M(4)
+
+
+/*
+* 제한 시간 : 4ms / 1s
+* 메모리 제한 : 2020KB / 512MB
+* 정답 비율 : 78.866%
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+
+int a[9];
+bool visited[9];
+
+void Func(int start, int cnt) {
+	if (cnt == m) {
+		for (int i = 0; i < m; i++) {
+			cout << a[i] << ' ';
+		}
+		cout << '\n';
+
+		return;
+	}
+
+	for (int i = start; i <= n; i++) {
+		visited[i] == true;
+		a[cnt] = i;
+		Func(i, cnt + 1);
+		visited[i] = false;
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> n >> m;
+
+	Func(1, 0);
+
+	return 0;
+}
