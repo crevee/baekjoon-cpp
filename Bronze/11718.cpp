7d651fa1ff0e56ab09078691e170005d215ea3c6
Ha Sangwook <64821752+crevee@users.noreply.github.com>
2024-01-09 15:55:42 +0900

    Bronze 5

---
+++ Bronze/11718.cpp
@@ -0,0 +1,26 @@
+//BaekJoon_11718
+//그대로 출력하기
+
+/*
+* 제한 시간 : 0ms / 1s
+* 메모리 제한 : 2024KB / 256MB
+* 정답 비율 : 32.861%
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	string s;
+	
+	while (getline(cin, s)) {
+		cout << s << '\n';
+	}
+	
+	return 0;
+}
