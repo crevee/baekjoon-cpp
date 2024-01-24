b1e4f094f190848aa3f7f4442634f0ee562fe425
Ha Sangwook <64821752+crevee@users.noreply.github.com>
2024-01-25 01:21:38 +0900

    Bronze 4

--- Bronze/11720.cpp
+++ Bronze/11720.cpp
@@ -1,28 +1,32 @@
 //BaekJoon_11720
-//팩토리얼
+//숫자의 합
 
 /*
 * 제한 시간 : 0ms / 1s
 * 메모리 제한 : 2024KB / 256MB
-* 정답 비율 : 54.677%
+* 정답 비율 : 55.644%
 */
 
 #include <iostream>
 #include <string>
 
+using namespace std;
+
 int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
 
-	int N = 0, answer = 0;
-	std::string list;
-	
-	std::cin >> N >> list;
+	int n;
+	long long result = 0;
+	string s;
 
+	cin >> n >> s;
 
-	for (int i = 0; i < N; i++)
-	{
-		answer += (list[i]) - '0';	//'0' 아스키 48
+	for (int i = 0; i < n; i++) {
+		result += s[i] - '0';
 	}
-	std::cout << answer;
+
+	cout << result;
 
 	return 0;
 }
