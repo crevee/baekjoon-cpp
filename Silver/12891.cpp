a2de7d1a0f655557c680c0f287c10cfe3fd28336
Ha Sangwook <64821752+crevee@users.noreply.github.com>
2024-01-26 04:10:31 +0900

    Silver 2

--- Silver/12891.cpp
+++ Silver/12891.cpp
@@ -2,10 +2,124 @@
 //DNA 비밀번호
 
 /*
+* 제한 시간 : 16ms / 2s
+* 메모리 제한 : 3680KB / 512MB
+* 정답 비율 : 33.935%
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int min_alph[4];
+int my_alph[4];
+
+int check_result = 0;
+
+void Add(char c) {
+	if (c == 'A') {
+		my_alph[0]++;
+
+		if (my_alph[0] == min_alph[0]) {
+			check_result++;
+		}
+	}
+	else if (c == 'C') {
+		my_alph[1]++;
+
+		if (my_alph[1] == min_alph[1]) {
+			check_result++;
+		}
+	}
+	else if (c == 'G') {
+		my_alph[2]++;
+
+		if (my_alph[2] == min_alph[2]) {
+			check_result++;
+		}
+	}
+	else if (c == 'T') {
+		my_alph[3]++;
+
+		if (my_alph[3] == min_alph[3]) {
+			check_result++;
+		}
+	}
+}
+
+void Remove(char c) {
+	if (c == 'A') {
+		if (my_alph[0] == min_alph[0]) {
+			check_result--;
+		}
+		my_alph[0]--;
+	}
+	else if (c == 'C') {
+		if (my_alph[1] == min_alph[1]) {
+			check_result--;
+		}
+		my_alph[1]--;
+	}
+	else if (c == 'G') {
+		if (my_alph[2] == min_alph[2]) {
+			check_result--;
+		}
+		my_alph[2]--;
+	}
+	else if (c == 'T') {
+		if (my_alph[3] == min_alph[3]) {
+			check_result--;
+		}
+		my_alph[3]--;
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int s, p, result = 0;
+	string str;
+
+	cin >> s >> p;
+	cin >> str;
+
+	for (int i = 0; i < 4; i++) {
+		cin >> min_alph[i];
+
+		if (min_alph[i] == 0) {
+			check_result++;
+		}
+	}
+
+	for (int i = 0; i < p; i++) {
+		Add(str[i]);
+	}
+
+	if (check_result == 4) {
+		result++;
+	}
+
+	for (int right = p; right < s; right++) {
+		int left = right - p;
+
+		Add(str[right]);
+		Remove(str[left]);
+
+		if (check_result == 4) {
+			result++;
+		}
+	}
+	cout << result;
+}
+
+
+/* 방법 2
+
 * 제한 시간 : 116ms / 2s
 * 메모리 제한 : 3684KB / 512MB
 * 정답 비율 : 33.834%
-*/
 
 #include <iostream>
 #include <map>
@@ -75,3 +189,5 @@ int main(){
     
     return 0;
 }
+
+*/
